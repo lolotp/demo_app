73457f06bee3ba6e3576ff47b1f3af6b097381bb
lolotp <lolo_vn@yahoo.co.uk>
2013-03-18 00:11:37 +0800

    changed output of get_posts for easier parsing :P This is only temporary

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,8 +8,18 @@ class UsersController < ApplicationController
 
   def get_posts
     respond_to do |format|
+      ret_str = ""
+      init = true
+      current_user.microposts.each do |s|
+        if init
+          ret_str = s[:content]
+          init = false
+        else
+          ret_str = ret_str + "#" + s[:content]
+        end
+      end
       if (signed_in?)
-        format.html { render :json => current_user.microposts }
+        format.html { render :text => ret_str }
       else
         format.html { render :text => "Unauthorized", :status => 401 }
       end
