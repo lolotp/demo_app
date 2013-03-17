c8179a2d0009ed36cf524104cdc474d867cfd4c0
lolotp <lolo_vn@yahoo.co.uk>
2013-03-18 00:47:41 +0800

    fixed bug in checking for user logging in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,17 +8,17 @@ class UsersController < ApplicationController
 
   def get_posts
     respond_to do |format|
-      ret_str = ""
-      init = true
-      current_user.microposts.each do |s|
-        if init
-          ret_str = s[:content]
-          init = false
-        else
-          ret_str = ret_str + "#" + s[:content]
-        end
-      end
       if (signed_in?)
+        ret_str = ""
+        init = true
+        current_user.microposts.each do |s|
+          if init
+            ret_str = s[:content]
+            init = false
+          else
+            ret_str = ret_str + "#" + s[:content]
+          end
+        end
         format.html { render :text => ret_str }
       else
         format.html { render :text => "Unauthorized", :status => 401 }
