abd8e7e5fb9ac66009c274a0a17e6fd00b18d2c4
Trinh Tuan Phuong <phuong@FOX-TWO-1.local>
2013-05-29 00:15:57 +0800

    added code so that user model can use helper

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
+include ActionView::Helpers
 
 class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
