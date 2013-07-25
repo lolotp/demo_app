55e8deef3774bc86722f350070293bd8aa44885b
Trinh Tuan Phuong <phuong@FOX-TWO-1.local>
2013-07-26 02:09:19 +0800

    fixed bug with follow

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -15,10 +15,10 @@ class FollowsController < ApplicationController
 	end
 
 	def destroy
-		f = Follow.find(params[:id])
+		f = current_user.follows.find_by_id(params[:id])
     @user = f.followee
     f.destroy
-		follow = Follow.find(params[:id])
+		follow = current_user.follows.find_by_id(params[:id])
 		respond_to do |format|
       format.html { redirect_to @user }
       format.js
