535bef1669c1695db0bdf53f945c2c5438b4f350
Trinh Tuan Phuong <phuong@FOX-TWO-1.local>
2013-08-02 04:45:24 +0800

    implemented job to inform user of when somebody comment on his/her own posts

--- app/models/inform_user_of_new_comment_resque_job.rb
+++ app/models/inform_user_of_new_comment_resque_job.rb
@@ -1,35 +1,46 @@
 class InformUserOfNewCommentResqueJob
-  def perform(comment, at_time)
+  
+  @queue = "comment_notification"
+
+  def self.perform(comment_id, at_time, should_inform_owner)
+    puts "beginning job"
+    comment = Comment.find_by_id(comment_id)
+    return unless comment
     post = comment.post
     commenter = comment.user
     post_owner = post.user
-    
+    at_time = comment.created_at
+    puts "beginning posting notifications"
     #firstly post a notification to post owner
-    if (commenter.id != post_owner.id)
+    if (commenter.id != post_owner.id and should_inform_owner)
       notification = post_owner.notifications.build( :content => "<n><a href=\"memcap://users/#{commenter.id}\" >#{commenter.name}</a> commented on your <a href=\"memcap://posts/#{post.id}\">post</a> <a href=\"memcap://comments/#{comment.id}\"/></n>", :viewed => false)
-      notification.save #do not process save failure, assume that failure to save notification is rare and it is ok for user to sometimes missthe notification
-    end
+      if (notification.save)
+        puts("successfully saved notification to post owner")
+      else
+        Resque.enqueue(self, comment_id, at_time, should_inform_owner)
+      end #if not.save
+    end #if commenter.id
     
+    puts "posting to user who commented before "
+    puts at_time
+    should_inform_owner = false;
     #then post all notifications who participate in a post discussion
     page = 1
     begin
       #query all users that commented on the post before this new comment is added
-      users = User.where("exists created_at < :at_time", :at_time => at_time).paginate(:page => page)
+      users = User.commented_on(post,at_time).paginate(:page => page)
       users.each do |user|
-        notification = user_owner.notifications.build( :content => "<n><a href=\"memcap://users/#{commenter.id}\" >#{commenter.name}</a> commented on a <a href=\"memcap://posts/#{post.id}\">post</a> you commented on. <a href=\"memcap://comments/#{comment.id}\"/></n>", :viewed => false)
+        notification = user.notifications.build( :content => "<n><a href=\"memcap://users/#{commenter.id}\" >#{commenter.name}</a> commented on a <a href=\"memcap://posts/#{post.id}\">post</a> you commented on. <a href=\"memcap://comments/#{comment.id}\"/></n>", :viewed => false)
         
         if (notification.save)
           at_time = comment.created_at
-        end#do not process save failure, assume that failure to save notification is rare and it is ok for user to sometimes miss the notification
-      end
-      page = page + 1
-      #increase page to pull the next set of comments
-    end while comments.any?
+        else
+          Resque.enqueue(self, comment_id, at_time, should_inform_owner)
+        end #if not.save
+      end #users.each do
+    end while comments.any? #begin
   rescue Resque::TermException
-    Resque.enqueue(self, comment, at_time)
-  end
+    Resque.enqueue(self, comment_id, at_time, should_inform_owner)
+  end #def/rescue
 
-  def on_failure_retry(e, *args)
-    Resque.enqueue self, *args
-  end
 end
