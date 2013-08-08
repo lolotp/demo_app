0a7c149852e2039ce8b9e1bc174720f93c859740
Trinh Tuan Phuong <phuong@FOX-TWO-1.local>
2013-08-09 01:59:37 +0800

    changed some migration in creation of table

--- db/migrate/20130808171250_create_user_location_feeds.rb
+++ db/migrate/20130808171250_create_user_location_feeds.rb
@@ -1,5 +1,5 @@
 class CreateUserLocationFeeds < ActiveRecord::Migration
-  def change
+  def up
     create_table :user_location_feeds do |t|
       t.integer :post_id
       t.integer :user_id
@@ -12,5 +12,12 @@ class CreateUserLocationFeeds < ActiveRecord::Migration
 
     add_index :user_location_feeds, :post_id, :unique => true
     add_index :user_location_feeds, :user_id
+    execute "CREATE INDEX user_location_feeds_location_index ON user_location_feeds USING gist(ll_to_earth(latitude,longitude))"
+  end
+  def down
+    execute "DROP INDEX user_location_feeds_location_index"
+    remove_index :user_location_feeds, :user_id
+    remove_index :user_location_feeds, :post_id
+    drop_table :user_location_feeds
   end
 end
