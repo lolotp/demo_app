87246c8dcef2f0be61cab42beeb25d593c63e3ea
Trinh Tuan Phuong <phuong@FOX-TWO-1.local>
2013-08-16 21:26:06 +0800

    only send notifications to people who are added

--- app/models/friendship_observer.rb
+++ app/models/friendship_observer.rb
@@ -4,7 +4,9 @@ class FriendshipObserver < ActiveRecord::Observer
   def after_create(friendship)
     receiver = friendship.friend
     adder = friendship.user
-    notification = receiver.notifications.build( :content => "<a href=\"memcap://users/#{adder.id}\" >#{adder.name}</a> added you as friend", :viewed => false)
-    notification.save
+    if friendship.status == "pending"
+      notification = receiver.notifications.build( :content => "<a href=\"memcap://users/#{adder.id}\" >#{adder.name}</a> added you as friend", :viewed => false)
+      notification.save
+    end
   end
 end
