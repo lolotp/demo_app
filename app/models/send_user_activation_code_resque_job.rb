2989748c3f84ba9b211f3e2737c3d4bc7ae195c6
Trinh Tuan Phuong <phuong@FOX-TWO-1.local>
2013-08-26 11:59:45 +0800

    quick fix to remove leading 0 from phone number for Taiwan, will break for Bangladesh user

--- app/models/send_user_activation_code_resque_job.rb
+++ app/models/send_user_activation_code_resque_job.rb
@@ -8,6 +8,11 @@ class SendUserActivationCodeResqueJob
   @retry_limit = 3
 
   def self.perform(user_id, phone_number)
+    phone_number = phone_number.to_s
+    if phone_number.start_with?("880") or phone_number.start_with?("650")
+      phone_number = phone_number[0,2] + phone_number[3, phone_number.length - 3]
+      puts "new phone number is " + phone_number.to_s
+    end
     user = User.find(user_id)
     params = {:app_id => ENV["HOIIO_APP_ID"],
               :access_token => ENV["HOIIO_ACCESS_TOKEN"],
@@ -17,7 +22,7 @@ class SendUserActivationCodeResqueJob
     url = "https://secure.hoiio.com/open/sms/send?" + params.to_query
     json = ActiveSupport::JSON.decode( open url )
     if json["status"] != "success_ok"
-      raise "job error can't send sms reason " + json.to_s
+      raise "job error can't send sms to " + phone_number.to_s + " reason " + json.to_s
     end
   end
 
