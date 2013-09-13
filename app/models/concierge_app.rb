4f4c00dc70722ef2143f90d3fea478d21c1e81cd
Trinh Tuan Phuong <phuong@FOX-TWO-1.local>
2013-09-14 01:09:01 +0800

    removed unneeded character

--- app/models/concierge_app.rb
+++ app/models/concierge_app.rb
@@ -1,7 +1,6 @@
 class ConciergeApp < ActiveRecord::Base
   attr_accessible :iso_country_code, :category, :app_store_link, :google_play_link, :link
   def as_json(options={})
-    Ω
     if (options[:iso_country_code])
       if /http:\/\/itunes.apple.com\/lookup\?id=[0-9]+/.match(self.app_store_link)
         s = self.app_store_link
