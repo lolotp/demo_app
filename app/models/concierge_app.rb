4a78c0f3e14bc65c54c7eecbe7d1c3302e6aca29
Trinh Tuan Phuong <phuong@FOX-TWO-1.local>
2013-09-14 01:06:25 +0800

    generate json at the end

--- app/models/concierge_app.rb
+++ app/models/concierge_app.rb
@@ -1,14 +1,14 @@
 class ConciergeApp < ActiveRecord::Base
   attr_accessible :iso_country_code, :category, :app_store_link, :google_play_link, :link
   def as_json(options={})
-    json_obj = super
+    Ω
     if (options[:iso_country_code])
       if /http:\/\/itunes.apple.com\/lookup\?id=[0-9]+/.match(self.app_store_link)
         s = self.app_store_link
         prefix = "http://itunes.apple.com/"
-        json_obj[:app_store_link] = s[0..prefix.length-1] + options[:iso_country_code] + "/" + s[prefix.length..-1]
+        self.app_store_link = s[0..prefix.length-1] + options[:iso_country_code] + "/" + s[prefix.length..-1]
       end
     end
-    json_obj
+    super
   end
 end
