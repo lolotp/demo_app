a9a7a23c313ffd15fa3c725147978d3066dd507f
Trinh Tuan Phuong <phuong@FOX-TWO-1.local>
2013-09-14 18:15:53 +0800

    only allow admin user

--- app/controllers/concierge_apps_controller.rb
+++ app/controllers/concierge_apps_controller.rb
@@ -1,6 +1,7 @@
 class ConciergeAppsController < ApplicationController
   before_filter :check_for_mobile, :only => [:index]
   before_filter :signed_in_user
+  before_filter :admin_user, :only => [:new, :edit, :update, :create, :destroy]
 
   def index
     if params[:iso_country_code]
@@ -17,6 +18,40 @@ class ConciergeAppsController < ApplicationController
       else
         format.json { render json: @concierge_apps }
       end
+      format.html {}
     end
-  end 
+  end
+
+  def edit
+    @concierge_app = ConciergeApp.find(params[:id])
+  end
+
+  def destroy
+    @concierge_app = ConciergeApp.find(params[:id])
+    @concierge_app.destroy
+    redirect_to concierge_apps_path
+  end
+
+  def new
+    @concierge_app = ConciergeApp.new
+  end
+  
+  def create
+    @concierge_app = ConciergeApp.new(params[:concierge_app])
+    if @concierge_app.save
+      redirect_to concierge_apps_path
+    else
+      redirect_to new_concierge_apps_path
+    end
+  end
+
+  def update
+    @concierge_app = ConciergeApp.find(params[:id])
+    if @concierge_app.update_attributes(params[:concierge_app])  
+      redirect_to concierge_apps_path
+    else
+      redirect_to edit_concierge_apps_path(@concierge_app)
+    end
+  end
+ 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -44,7 +44,7 @@ DemoApp::Application.routes.draw do
   resources :landmarks, only: [:create, :index, :show]
   resources :notifications, only: [:update, :create]
 	resources :follows, only: [:create, :destroy]
-  resources :concierge_apps, only: [:index]
+  resources :concierge_apps
 
   root to: 'static_pages#home'
 
