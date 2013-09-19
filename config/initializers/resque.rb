7bcb9a5c35e2a462a765e88385d1d72d4b5737a1
Trinh Tuan Phuong <phuong@FOX-TWO-1.local>
2013-09-19 20:06:52 +0800

    reconfigure resque for worker

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -2,3 +2,4 @@ ENV["REDISTOGO_URL"] ||= "redis://localhost:6379/"
 
 uri = URI.parse(ENV["REDISTOGO_URL"])
 REDIS_WORKER = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+Resque.redis = REDIS_WORKER
