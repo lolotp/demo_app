28324e11f125d3be0d32d22458335d4e4e86c18e
Trinh Tuan Phuong <phuong@FOX-TWO-1.local>
2014-02-03 12:43:21 +0700

    adjusted notifications for easier parsing

--- app/models/inform_user_of_new_comment_resque_job.rb
+++ app/models/inform_user_of_new_comment_resque_job.rb
@@ -13,7 +13,7 @@ class InformUserOfNewCommentResqueJob
     puts "beginning posting notifications"
     #firstly post a notification to post owner
     if (commenter.id != post_owner.id and should_inform_owner)
-      notification = post_owner.notifications.build( :content => "<n><a href=\"memcap://users/#{commenter.id}\" >#{commenter.name}</a> commented on your <a href=\"memcap://posts/#{post.id}\">post</a> <a href=\"memcap://comments/#{comment.id}\"/></n>", :viewed => false)
+      notification = post_owner.notifications.build( :content => "<n5><a href=\"memcap://users/#{commenter.id}\" >#{commenter.name}</a> commented on your <a href=\"memcap://posts/#{post.id}\">post</a> <a href=\"memcap://comments/#{comment.id}\"/></n5>", :viewed => false)
       if (notification.save)
         puts("successfully saved notification to post owner")
       else
@@ -22,7 +22,7 @@ class InformUserOfNewCommentResqueJob
     end #if commenter.id
 
     #send notifications to commenter
-    notification = commenter.notifications.build( :content => "<n>You commented on <a href=\"memcap://posts/#{post.id}\">post</a> <a href=\"memcap://comments/#{comment.id}\"/></n>", :viewed => false)
+    notification = commenter.notifications.build( :content => "<n4>You commented on a <a href=\"memcap://posts/#{post.id}\">post</a> <a href=\"memcap://comments/#{comment.id}\"/></n4>", :viewed => false)
     notification.save
     
     puts "posting to user who commented before "
@@ -34,6 +34,9 @@ class InformUserOfNewCommentResqueJob
       #query all users that commented on the post before this new comment is added
       comments_from_unqiue_users = Comment.on_post_by_unqiue_users(post,at_time).paginate(:page => page)
       comments_from_unqiue_users.each do |comment|
+        if (comment.user_id == post_owner.id)
+          next
+        end
         notification = Notification.new( :content => "<n><a href=\"memcap://users/#{commenter.id}\" >#{commenter.name}</a> commented on a <a href=\"memcap://posts/#{post.id}\">post</a> you commented on. <a href=\"memcap://comments/#{comment.id}\"/></n>", :viewed => false)
         notification.receiver_id = comment.user_id
         if (notification.save)
