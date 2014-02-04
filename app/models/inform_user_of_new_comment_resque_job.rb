7e5038821f067c847bffac9209db7748ea5f5e91
Trinh Tuan Phuong <phuong@FOX-TWO-1.local>
2014-02-04 11:47:04 +0700

    fixed new comment job hanging

--- app/models/inform_user_of_new_comment_resque_job.rb
+++ app/models/inform_user_of_new_comment_resque_job.rb
@@ -32,18 +32,17 @@ class InformUserOfNewCommentResqueJob
     page = 1
     begin
       #query all users that commented on the post before this new comment is added
-      comments_from_unqiue_users = Comment.on_post_by_unqiue_users(post,at_time).paginate(:page => page)
-      comments_from_unqiue_users.each do |comment|
-        if (comment.user_id == post_owner.id)
-          next
-        end
-        notification = Notification.new( :content => "<n><a href=\"memcap://users/#{commenter.id}\" >#{commenter.name}</a> commented on a <a href=\"memcap://posts/#{post.id}\">post</a> you commented on. <a href=\"memcap://comments/#{comment.id}\"/></n>", :viewed => false)
-        notification.receiver_id = comment.user_id
-        if (notification.save)
-          at_time = comment.created_at
-        else
-          Resque.enqueue(self, comment_id, at_time, should_inform_owner)
-        end #if not.save
+      comments_from_unique_users = Comment.on_post_by_unqiue_users(post,at_time).paginate(:page => page)
+      comments_from_unique_users.each do |comment|
+        if (comment.user_id != post_owner.id)
+          notification = Notification.new( :content => "<n><a href=\"memcap://users/#{commenter.id}\" >#{commenter.name}</a> commented on a <a href=\"memcap://posts/#{post.id}\">post</a> you commented on. <a href=\"memcap://comments/#{comment.id}\"/></n>", :viewed => false)
+          notification.receiver_id = comment.user_id
+          if (notification.save)
+            at_time = comment.created_at
+          else
+            Resque.enqueue(self, comment_id, at_time, should_inform_owner)
+          end #if not.save
+        end # if id is different
       end #comments_from_unqiue_users.each do
     end while comments_from_unqiue_users.any? #begin
     puts "job ended sucessfully"
