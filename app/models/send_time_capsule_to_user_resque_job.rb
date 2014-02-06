da8c6765479a6b9853f74e6a21390460ba874448
Trinh Tuan Phuong <phuong@FOX-TWO-1.local>
2014-02-06 11:45:16 +0700

    added push notification functionality

--- app/models/send_time_capsule_to_user_resque_job.rb
+++ app/models/send_time_capsule_to_user_resque_job.rb
@@ -17,6 +17,16 @@ class SendTimeCapsuleToUserResqueJob
       notification = to_user.notifications.build( :content => "<n3><a href=\"memcap://users/#{from_user.id}\" >#{from_user.name}</a> sent you a <a href=\"memcap://posts/#{post.id}\">time capsule</a></n3>", :viewed => false)
       if (notification.save)
           puts("successfully saved notification to post owner")
+          uri = URI.parse(ENV['PUSH_URL'])
+          http = Net::HTTP.new(uri.host, uri.port)
+          http.use_ssl = true
+          http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+          message = "#{from_user.name} sent you a time capsule"
+          message_url = "memcap://posts/#{post.id}"
+          data = { :appid => ENV["PUSH_APPID"], :username => ENV["PUSH_USERNAME"], :password => ENV["PUSH_PASSWORD"], :message => message, :messageurl => message_url, :id_fordevice => to_user.email }
+          request = Net::HTTP::Post.new(uri.path, {})
+          request.body = data.to_query
+          response = http.request(request)
       else
         raise "failure can't send notification"
       end
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,9 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 DemoApp::Application.initialize!
+
+#custom environment variables
+ENV["PUSH_APPID"] = 'F0000010'
+ENV["PUSH_USERNAME"] = 'foxtwo'
+ENV["PUSH_PASSWORD"] = 'foxtwo'
+ENV["PUSH_URL"]='https://mpush.foxcradle.com/push/singlepush.php'
